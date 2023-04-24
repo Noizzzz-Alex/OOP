4b7ac7026b8d962df2db40039d759dc1a6b8d4ad
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-04-24 22:30:42 +0700

    init seminar#3

--- Main.java
+++ Main.java
@@ -5,24 +5,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik");
-        Cat cat1 = new Cat("Barsuk");
-        Cat cat2 = new Cat("Admiral");
-        Cat cat3 = new Cat("Baloo");
-        Cat cat4 = new Cat("Baron");
-        Cat cat5 = new Cat("Benedict");
-        Cat cat6 = new Cat("Blitz");
-        Cat cat7 = new Cat("Boomer");
-        Cat cat8 = new Cat("Caesar");
-        Cat cat9 = new Cat("Carter");
-        Cat cat10 = new Cat("Cooper");
-        Plate plate = new Plate(30);
-        List<Cat>cats = new ArrayList<>();
-        cats.add(cat);cats.add(cat1);cats.add(cat2);cats.add(cat3);cats.add(cat4);cats.add(cat5);cats.add(cat6);
-        cats.add(cat7);cats.add(cat8);cats.add(cat9);cats.add(cat10);
-        mainMenu.Menu(cats,plate);
-
-
 
     }
 }
