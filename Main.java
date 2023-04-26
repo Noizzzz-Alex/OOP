e4c79ac180263a3acc9a30e6c953a1c4e859b7c2
Noizzzz1990 <Noizzzz1990@gmail.com>
2023-04-27 02:54:44 +0700

    added classes Fruit,Apple,Orange,Box

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 
 public class Main {
 
@@ -6,4 +6,29 @@ public class Main {
     public static void main(String[] args) {
 
     }
+}
+abstract class Fruit{
+    private final float weight;
+
+
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+}
+class Apple extends Fruit{
+
+    public Apple(float weight) {
+        super(weight);
+    }
+}
+class Orange extends Fruit {
+
+    public Orange(float weight) {
+        super(weight);
+    }
+}
+class Box<T extends Fruit>{
+    ArrayList<T> fruits;
+
 }
