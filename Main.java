c9997ad8dfc065e73d3530164dfb7379e6348720
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-04-30 23:24:20 +0700

    seminar#5 init

--- Main.java
+++ Main.java
@@ -1,42 +1,7 @@
 public class Main {
-    /*
-    a. Даны классы Fruit, Apple extends Fruit, Orange extends Fruit;
-b. Класс Box, в который можно складывать фрукты. Коробки условно сортируются по типу фрукта,
-поэтому в одну коробку нельзя сложить и яблоки, и апельсины;
-c. Для хранения фруктов внутри коробки можно использовать ArrayList;
-d. Сделать метод getWeight(), который высчитывает вес коробки, зная вес одного фрукта и их количество:
-вес яблока – 1.0f, апельсина – 1.5f (единицы измерения не важны);
-e. Внутри класса Box сделать метод compare(), который позволяет сравнить текущую коробку с той, которую
-подадут в compare() в качестве параметра. true – если их массы равны, false в противоположном случае.
-Можно сравнивать коробки с яблоками и апельсинами;
-f. Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую.
-Помним про сортировку фруктов: нельзя яблоки высыпать в коробку с апельсинами.
-Соответственно, в текущей коробке фруктов не остается, а в другую перекидываются объекты, которые были в первой;
-g. Не забываем про метод добавления фрукта в коробку.
-     */
 
 
     public static void main(String[] args) {
-        mainMenu.Menu();
-//        Apple a = new Apple(200);
-//        Apple a1 = new Apple(200);
-//        Apple a2 = new Apple(200);
-//        Apple a3 = new Apple(200);
-//        Apple a4 = new Apple(200);
-//        Orange o = new Orange(250);
-//
-//        Box<Apple> appleBox = new Box<>();
-//        Box<Orange> orangeBox = new Box<>();
-//        Box<Orange> orange = new Box<>();
-//        appleBox.addFruit(a);
-//        appleBox.addFruit(a1);
-//        appleBox.addFruit(a2);
-//        appleBox.addFruit(a3);
-//        appleBox.addFruit(a4);
-//        System.out.println(appleBox);
-//        System.out.println(appleBox.getFruitName());
-//
-
     }
 }
 
