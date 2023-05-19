6d7cd23fedc72877b2afe961b692ed4c1da99875
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-05-20 01:52:04 +0700

    added Order

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        System.out.println("Введите заказ:");
+        Order order = new Order("", "", 0, 0);
+        order.inputFromConsole();
+        order.saveToJson();
     }
 }
 
