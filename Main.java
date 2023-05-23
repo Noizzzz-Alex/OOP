724c831028695c24f39ac1cdb23246aeefcb2875
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-05-23 14:03:55 +0700

    init seminar#7

--- Main.java
+++ Main.java
@@ -4,13 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ReportCalculator reportCalculator = new ReportCalculator();
-        ReportOutput reportOutput = new PrintReport();
-        ReportGenerator reportGenerator = new ReportGenerator(reportCalculator, reportOutput);
-        Report report = new Report(reportCalculator.calculate());
-        reportGenerator.generateReport();
-        System.out.println(report);
-
     }
 }
 
--- Report.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Report {
-    private List<ReportItem> items;
-
-    public Report(List<ReportItem> items) {
-        this.items = items;
-    }
-}
